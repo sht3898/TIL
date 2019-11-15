61c058bc60a122e9be0ffae5cc3fe19ddd1e15fc
unknown <sht3898>
2019-11-15 16:55:48 +0900

    191115

--- vue/newvue/todo-vue-cli/src/App.vue
+++ vue/newvue/todo-vue-cli/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <!-- 컴포넌트를 호출 -->
     <TodoList category="싸피"/>   <!-- 변경 되었음 -->
     <TodoList category="취업"/>   <!-- 변경 되었음 -->
     <TodoList category="개인"/>   <!-- 변경 되었음 -->
@@ -8,12 +9,13 @@
 </template>
 
 <script>
-import TodoList from './components/TodoList.vue'
+// 1. 컴포넌트를 부모 컴포넌트에서 불러옴.
+import TodoList from './components/TodoList.vue'  // 변경
 
 export default {
   name: 'app',
   components: {
-    TodoList  // 변경되었음
+    TodoList  // 변경되었음 2. 컴포넌트를 추가
   }
 }
 </script>
