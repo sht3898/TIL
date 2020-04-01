df3f2e06e02d74b76687cfe0abebcb32ac763ece
sht3898 <sehyunteg@naver.com>
2020-04-02 02:43:23 +0900

    200402 Night | Java

---
+++ java/algorithm/src/intro/hello.java
@@ -0,0 +1,15 @@
+package intro;
+
+import java.util.Scanner;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String message = new String();
+		message = sc.next();
+		System.out.println(message);
+	}	
+
+}
