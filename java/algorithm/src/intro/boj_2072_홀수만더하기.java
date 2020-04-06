f1f98d46f19413ed93df29f4c34bdbf32c4755c6
sht3898 <sehyunteg@naver.com>
2020-04-06 19:10:09 +0900

    200406 | Evening

---
+++ java/algorithm/src/intro/boj_2072_홀수만더하기.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
+import java.util.StringTokenizer;
+import java.io.File;
+import java.io.FileReader;
+
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		/*
+		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
+		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
+		 */
+		File file = new File("2072_input.txt");
+		FileReader filereader = new FileReader(file);
+		int singleCh = 0;
+		while((singleCh = filereader.read()) != -1) {
+			System.out.println((char)singleCh);
+		}
+		filereader.close();
+	}
+}
