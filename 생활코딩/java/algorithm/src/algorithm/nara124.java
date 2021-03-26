3b12fcc618bc57d064e95fbd3b977939fe034349
sht3898 <sehyunteg@naver.com>
2021-03-26 23:27:13 +0900

    210326 | add

---
+++ 생활코딩/java/algorithm/src/algorithm/nara124.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class nara124 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		sc.close();
+
+
+	}
+
+}
